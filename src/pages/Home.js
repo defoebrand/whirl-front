bfa441a020bda32b1208118618a83e62e2118c93
Brandon <defoe.brand@gmail.com>
2021-04-17 23:12:28 +0300

    Copy and clean trending section of marketplace home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,109 @@
 const Home = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Edit
-        <code>src/App.js</code>
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <section className="__first-marketplace-section">
+    <div className="__outer-sub-section">
+      <h1 className="__center-title">Trending</h1>
+      <div className="__center-sub-section">
+        <a href="www.google.com" className="__advertisement-carousel">
+          <div className="inner-carousel">
+            <div className="__top">
+              <button type="button">
+                <img src="../multimedia/img/icons/marketplace/followers/100k.png" alt="" />
+              </button>
+              <button type="button">
+                <img src="../multimedia/img/icons/social/light/__tiktok-logo.png" alt="" />
+              </button>
+            </div>
+            <div className="__bottom">
+              <img src="../multimedia/img/icons/marketplace/yourlogohere.png" alt="" />
+              <h3>$25,000</h3>
+            </div>
+          </div>
+          <p>@username</p>
+        </a>
+        <a href="www.google.com" className="__advertisement-carousel">
+          <div className="inner-carousel">
+            <div className="__top">
+              <button type="button">
+                <img src="../multimedia/img/icons/marketplace/followers/5m.png" alt="" />
+              </button>
+              <button type="button">
+                <img src="../multimedia/img/icons/social/light/__youtube-logo.png" alt="" />
+              </button>
+            </div>
+            <div className="__bottom">
+              <img src="../multimedia/img/icons/marketplace/yourlogohere.png" alt="" />
+              <h3>$25,000</h3>
+            </div>
+          </div>
+        </a>
+        <a href="www.google.com" className="__advertisement-carousel">
+          <div className="inner-carousel">
+            <div className="__top">
+              <button type="button">
+                <img src="../multimedia/img/icons/marketplace/followers/10k.png" alt="" />
+              </button>
+              <button type="button">
+                <img src="../multimedia/img/icons/social/light/__instagram-logo.png" alt="" />
+              </button>
+            </div>
+            <div className="__bottom">
+              <img src="../multimedia/img/icons/marketplace/yourlogohere.png" alt="" />
+              <h3>$25,000</h3>
+            </div>
+          </div>
+        </a>
+        <a href="www.google.com" className="__advertisement-carousel">
+          <div className="inner-carousel">
+            <div className="__top">
+              <button type="button">
+                <img src="../multimedia/img/icons/marketplace/followers/10m.png" alt="" />
+              </button>
+              <button type="button">
+                <img src="../multimedia/img/icons/social/light/__twitter-logo.png" alt="" />
+              </button>
+            </div>
+            <div className="__bottom">
+              <img src="../multimedia/img/icons/marketplace/yourlogohere.png" alt="" />
+              <h3>$25,000</h3>
+            </div>
+          </div>
+        </a>
+        <a href="www.google.com" className="__advertisement-carousel">
+          <div className="inner-carousel">
+            <div className="__top">
+              <button type="button">
+                <img src="../multimedia/img/icons/marketplace/followers/10m.png" alt="" />
+              </button>
+              <button type="button">
+                <img src="../multimedia/img/icons/social/light/__snap-logo.png" alt="" />
+              </button>
+            </div>
+            <div className="__bottom">
+              <img src="../multimedia/img/icons/marketplace/yourlogohere.png" alt="" />
+              <h3>$25,000</h3>
+            </div>
+          </div>
+        </a>
+        <a href="www.google.com" className="__advertisement-carousel">
+          <div className="inner-carousel">
+            <div className="__top">
+              <button type="button">
+                <img src="../multimedia/img/icons/marketplace/followers/10m.png" alt="" />
+              </button>
+              <button type="button">
+                <img src="../multimedia/img/icons/social/light/__fb-logo.png" alt="" />
+              </button>
+            </div>
+            <div className="__bottom">
+              <img src="../multimedia/img/icons/marketplace/yourlogohere.png" alt="" />
+              <h3>$25,000</h3>
+            </div>
+          </div>
+        </a>
+
+      </div>
+    </div>
+  </section>
 );
 
 export default Home;
