65dbd2ab6a9233e0fb6f459f94fe7dd9fc7d2c7a
Brandon <defoe.brand@gmail.com>
2021-04-17 23:23:03 +0300

    Create Advertisement component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,11 @@
+import Advertisement from '../components/Advertisement';
+
 const Home = () => (
   <section className="__first-marketplace-section">
     <div className="__outer-sub-section">
       <h1 className="__center-title">Trending</h1>
       <div className="__center-sub-section">
-        <a href="www.google.com" className="__advertisement-carousel">
-          <div className="inner-carousel">
-            <div className="__top">
-              <button type="button">
-                <img src="../multimedia/img/icons/marketplace/followers/100k.png" alt="" />
-              </button>
-              <button type="button">
-                <img src="../multimedia/img/icons/social/light/__tiktok-logo.png" alt="" />
-              </button>
-            </div>
-            <div className="__bottom">
-              <img src="../multimedia/img/icons/marketplace/yourlogohere.png" alt="" />
-              <h3>$25,000</h3>
-            </div>
-          </div>
-          <p>@username</p>
-        </a>
+        <Advertisement followers="../multimedia/img/icons/marketplace/followers/100k.png" platformLogo="../multimedia/img/icons/social/light/__tiktok-logo.png" companyLogo="../multimedia/img/icons/marketplace/yourlogohere.png" price="$25,000" />
         <a href="www.google.com" className="__advertisement-carousel">
           <div className="inner-carousel">
             <div className="__top">
